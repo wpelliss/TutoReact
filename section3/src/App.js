420952afea0f9d11e839e8e63dc802bfb073a6c1
Wilfried Pellissard <wilfried.pellissard@hotmail.fr>
2023-03-22 12:55:23 +0100

    3.15 - Découverte de useState()

--- section3/src/App.js
+++ section3/src/App.js
@@ -1,10 +1,16 @@
 import Item from './Item'
+import {useState} from 'react'
 
 function App() {
+
+  const [monState, setMonState] = useState(10);
+  console.log(monState);
+
   return (
     <div className="App">
       <h1>Hello app react</h1>
       <Item />
+      {monState}
     </div>
   );
 }
