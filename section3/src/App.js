106ef5244cfea71b729d331f770d7f6b9f46dd99
Wilfried Pellissard <wilfried.pellissard@hotmail.fr>
2023-03-24 13:02:55 +0100

    3.21 - Utiliser des inputs

--- section3/src/App.js
+++ section3/src/App.js
@@ -8,6 +8,8 @@ function App() {
   const incrementState = () => setMonState(monState + 1);
   const [darkMode, setDarkMode] = useState(true);
   const switchDarkMode = () => setDarkMode(darkMode ? false : true);
+  const [inputData, setInputData] = useState();
+  const changeInput = (e) => setInputData(e);
 
   return (
     <div className={`App ${darkMode ? "dark" : "light"}`}>
@@ -15,6 +17,8 @@ function App() {
       <Item txt="Changer le state" func={incrementState}/>
       <h1>Darkmode : {darkMode}</h1>
       <Item txt="Changer le mode" func={switchDarkMode}/>
+      <h1>Input : </h1><input type="text" onInput={e => changeInput(e.target.value)}></input>
+      <h2>{inputData}</h2>
     </div>
   );
 }
